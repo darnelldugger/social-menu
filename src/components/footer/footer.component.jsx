3bef4f3640d86cdcc3fb8ecf1e8acddb15c5150f
Darnell Dugger <darnelldugger@gmail.com>
2023-03-11 19:56:20 -0500

    fix navbar space

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
               />
           </Grid>
           <Grid item xs={12} sm={4} sx={{ textAlign: "center" }}>
-            <Typography variant="body1">Copyright - 2022</Typography>
+            <Typography variant="body1">Copyright - 2023</Typography>
           </Grid>
           <Grid item container xs={8} sm={2} sx={{ textAlign: "center" }}>
             <Grid item xs={4}>
--- src/components/nav-bar.component.jsx
+++ src/components/nav-bar.component.jsx
@@ -1,4 +1,4 @@
-import {  Box, Toolbar } from "@mui/material";
+import { Typography, Box, Toolbar } from "@mui/material";
 import ButtonSecondary from "./buttons/button-secondary.component";
 import AppBar from '@mui/material/AppBar';
 
@@ -9,11 +9,13 @@ const NavBar = () => {
     <Box sx={{ flexGrow: 1, mb: 5 }}>
       <AppBar position="static" sx={{ p: 1 }} style={{ backgroundColor: '#ffffff' }}>
         <Toolbar>
-          <img
-            src="https://res.cloudinary.com/nell1818/image/upload/v1678580542/MENUUI_1_fiabjv.png"
-            height={30}
-            alt="logo"
-          />
+          <Typography variant="h4" fontSize='30px' color='text.primary' component="div" sx={{ flexGrow: 1 }}>
+            <img
+              src="https://res.cloudinary.com/nell1818/image/upload/v1678580542/MENUUI_1_fiabjv.png"
+              height={30}
+              alt="logo"
+            />
+          </Typography>
           <ButtonSecondary />
         </Toolbar>
       </AppBar>
